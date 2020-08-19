c1003055d9474c417e1a8abddb408477acb8c5b5
MattRoger <mrogerphotography@gmail.com>
2020-08-19 15:02:00 -0600

    trying to hookup to jawsdb

--- inc/connection.php
+++ inc/connection.php
@@ -14,7 +14,7 @@
 try{
     $db = new PDO("mysql:host=l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;
     dbname=arrowverse_db;port=3306",
-    "s5qiw4adv2l8acoo","xmqf66ts9hwf8dml");    
+    "s5qiw4adv2l8acoo","r7s6mbj2txjrms8b");    
   
 }catch(
     Exception $e){
