2f5e8abd2c1049d5d09f8532b5a7e03918906cce
MattRoger <mrogerphotography@gmail.com>
2020-08-19 13:29:57 -0600

    added felicity, about to try and deploy the site

---
+++ seeds/arrow.sql
@@ -0,0 +1,117 @@
+-- Arrow
+-- Oliver Queen
+
+INSERT INTO Characters (
+main_id, first_name, last_name, main_alias, _show, alignment,species, 
+strength, attack, dexterity, intelligence, defense, img, bg_color)
+VALUES ( 101, "Oliver", "Queen",  "The Green Arrow", "arrow", "good", "human",
+7, 9, 8, 9, 7,
+'arrow/green_arrow.jpg',
+"#124222"
+);
+INSERT INTO AKA(AKA_id, main_id, aka)
+VALUES(1011 ,101, "The Hood");
+INSERT INTO AKA(AKA_id, main_id, aka)
+VALUES(1012 ,101, "The Vigalanti");
+INSERT INTO AKA(AKA_id, main_id, aka)
+VALUES(1013 ,101, "The Arrow");
+INSERT INTO AKA(AKA_id, main_id, aka)
+VALUES(1014 ,101, "The Spectre");
+
+-- Heros Skills
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(101, 1001);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(101, 1002);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(101, 1003);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(101, 1004);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(101, 1005);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(101, 1006);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(101, 1007);
+
+-- Diggle
+
+INSERT INTO Characters (
+main_id, first_name, last_name, main_alias, _show, alignment,species, 
+strength, attack, dexterity, intelligence, defense, img, bg_color)
+VALUES ( 102, "John", "Diggle",  "Spartan", "arrow", "good", "human",
+7, 8, 6, 7, 7,
+'arrow/spartan.jpg',
+"#470f27");
+
+-- hero skills
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(102, 1001);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(102, 1002);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(102, 1003);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(102, 1004);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(102, 1005);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(102, 1006);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(102, 1009);
+
+-- felicity
+
+INSERT INTO Characters (
+main_id, first_name, last_name, main_alias, _show, alignment,species, 
+strength, attack, dexterity, intelligence, defense, img, bg_color)
+VALUES ( 103, "Felicity", "Smoke",  "Overwatch", "arrow", "good", "human",
+7, 9, 8, 9, 7,
+'arrow/felicity.jpg',
+"#1d8d4d"
+);
+
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(103, 1007);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(103, 1010);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(103, 1011);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(103, 1012);
+
+-- slade wilson
+
+INSERT INTO Characters (
+main_id, first_name, last_name, main_alias, _show, alignment,species, 
+strength, attack, dexterity, intelligence, defense, img, bg_color)
+VALUES ( 104, "Slade", "Wilson",  "Deathstroke", "arrow", "bad", "Enhanced Human",
+7, 9, 8, 9, 7,
+'arrow/deathstroke.jpg',
+"#190136"
+);
+-- skills
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 1001);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 1003);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 1004);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 1005);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 1006);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 1007);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 2001);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 2002);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 2019);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 2020);
+INSERT INTO hero_skills(hero_id,skill_id)
+VALUES(104, 2021);
+
+
--- seeds/seed.sql
+++ seeds/seed.sql
@@ -41,103 +41,6 @@ CREATE TABLE hero_skills(
   skill_id int NOT NULL
 );
 
--- Arrow
--- Oliver Queen
-
-INSERT INTO Characters (
-main_id, first_name, last_name, main_alias, _show, alignment,species, 
-strength, attack, dexterity, intelligence, defense, img, bg_color)
-VALUES ( 101, "Oliver", "Queen",  "The Green Arrow", "arrow", "good", "human",
-7, 9, 8, 9, 7,
-'arrow/green_arrow.jpg',
-"#124222"
-);
-INSERT INTO AKA(AKA_id, main_id, aka)
-VALUES(1011 ,101, "The Hood");
-INSERT INTO AKA(AKA_id, main_id, aka)
-VALUES(1012 ,101, "The Vigalanti");
-INSERT INTO AKA(AKA_id, main_id, aka)
-VALUES(1013 ,101, "The Arrow");
-INSERT INTO AKA(AKA_id, main_id, aka)
-VALUES(1014 ,101, "The Spectre");
-
--- Heros Skills
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(101, 1001);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(101, 1002);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(101, 1003);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(101, 1004);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(101, 1005);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(101, 1006);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(101, 1007);
-
--- Diggle
-
-INSERT INTO Characters (
-main_id, first_name, last_name, main_alias, _show, alignment,species, 
-strength, attack, dexterity, intelligence, defense, img, bg_color)
-VALUES ( 102, "John", "Diggle",  "Spartan", "arrow", "good", "human",
-7, 8, 6, 7, 7,
-'arrow/spartan.jpg',
-"#470f27");
-
--- hero skills
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(102, 1001);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(102, 1002);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(102, 1003);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(102, 1004);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(102, 1005);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(102, 1006);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(102, 1009);
-
--- slade wilson
-
-INSERT INTO Characters (
-main_id, first_name, last_name, main_alias, _show, alignment,species, 
-strength, attack, dexterity, intelligence, defense, img, bg_color)
-VALUES ( 103, "Slade", "Wilson",  "Deathstroke", "arrow", "bad", "Enhanced Human",
-7, 9, 8, 9, 7,
-'arrow/deathstroke.jpg',
-"#190136"
-);
--- skills
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 1001);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 1003);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 1004);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 1005);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 1006);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 1007);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 1007);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 2001);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 2002);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 2019);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 2020);
-INSERT INTO hero_skills(hero_id,skill_id)
-VALUES(103, 2021);
 
 -- flash
 -- Barry
@@ -545,7 +448,7 @@ INSERT INTO skills(skill_id, skill_type, skill_name)
 VALUES(1011, "ability", "Master Engineer");
 
 INSERT INTO skills(skill_id, skill_type, skill_name)
-VALUES(1012, "ability", "Master mathematicia");
+VALUES(1012, "ability", "Master mathematician");
 
 INSERT INTO skills(skill_id, skill_type, skill_name)
 VALUES(1013, "ability", "Master thief");
