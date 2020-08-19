ad8b082a07412292ed9c6a488ae5f4252eabdb2e
MattRoger <mrogerphotography@gmail.com>
2020-08-19 16:42:05 -0600

    trying to hookup to jawsdb

--- inc/connection.php
+++ inc/connection.php
@@ -31,8 +31,7 @@ try{
 
 
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
+
 
 //$con->close();
 
